f4e60aaac03137fd21c279e8f8a74d208037642c
BVRIT <BVRIT@IT-LAB-77>
2020-02-15 12:46:24 +0530

    new test java added

--- PMS/src/main/java/com/cg/pms/pl/Test.java
+++ PMS/src/main/java/com/cg/pms/pl/Test.java
@@ -2,10 +2,8 @@ package com.cg.pms.pl;
 
 public class Test {
 
-	public static void main(String[] args)
-	{
-
-		System.out.println("Testing");
+	public static void main(String[] args) {
+System.out.println("testing");
 	}
 
 }
